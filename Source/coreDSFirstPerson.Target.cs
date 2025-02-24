0b8d67eece835489b2ac0fb5d4cf22ad95591859
Jean-Philippe Lebel <jp@lebel.pro>
2025-02-24 17:54:46 -0500

    Fix compilation warning due to IncludeOrderVersion flag

---
+++ Source/coreDSFirstPerson.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class coreDSFirstPersonTarget : TargetRules
+{
+	public coreDSFirstPersonTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.AddRange( new string[] { "coreDSFirstPerson" } );
+	}
+}
---
+++ Source/coreDSFirstPersonEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class coreDSFirstPersonEditorTarget : TargetRules
+{
+	public coreDSFirstPersonEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.AddRange( new string[] { "coreDSFirstPerson" } );
+	}
+}
